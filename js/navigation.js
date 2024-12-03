2044dadeac28ca23affd97c31e17364fd7f5de41
Bartosz Bednarski <bambocha1997@gmail.com>
2024-12-03 09:01:17 +0100

    Added logic for hamburger menu and changed styling.

--- js/navigation.js
+++ js/navigation.js
@@ -1,2 +1,15 @@
 const hamburgerButton = document.getElementById("hamburger-button");
-console.log(hamburgerButton);
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const hamburgerCloseButton = document.getElementById("hamburger-close-button");
+const hamburgerMenuLinks = hamburgerMenu.querySelectorAll("a");
+hamburgerButton.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("hamburger-menu__open");
+});
+hamburgerCloseButton.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("hamburger-menu__open");
+});
+hamburgerMenuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("hamburger-menu__open");
+  });
+});
